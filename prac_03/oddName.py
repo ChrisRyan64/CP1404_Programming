39ca6f0a6b3454083deae7778bf3c4c355861b73
ChrisRyan64 <christopher.ryan1@my.jcu.edu.au>
2017-08-23 23:59:27 +1000

    copy previous version

--- prac_03/oddName.py
+++ prac_03/oddName.py
@@ -1,7 +1,26 @@
 '''Christopher Ryan'''
-name = input("Enter name: ")
-name_length = len(name)
-while name_length == 0:
-    name = input("Invalid name: ")
-for i in range(1, name_length, 2):
-    print(name[i])
+def main():
+    name = get_name()
+    letter_frequency = int(input("Skip how many letters?"))
+    while letter_frequency <= 0:
+        try:
+            letter_frequency = int(input("Enter a value > 0"))
+        except ValueError:
+            letter_frequency = int(input("Enter a number"))
+    display_cut_name(name, letter_frequency)
+
+
+def display_cut_name(name, skip_frequency):
+    for i in range(0, len(name), skip_frequency):
+        print(name[i])
+
+
+def get_name():
+    name = input("Enter name: ")
+    name_length = len(name)
+    while name_length == 0:
+        name = input("Invalid name: ")
+    return name
+
+
+main()
