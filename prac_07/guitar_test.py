b789670b17a7ace45511959b0efd51243e5b8b4e
jc443785 <christopher.ryan1@my.jcu.edu.au>
2017-09-21 12:47:00 +1000

    complete from scratch

---
+++ prac_07/guitar_test.py
@@ -0,0 +1,13 @@
+from prac_07.guitars import Guitar
+
+
+def main():
+    example_vintage = Guitar("Gibson", 1922, 16001.95)
+    example_other = Guitar("Peasant", 2013, 2100)
+    print("Gibson get_age() - Expected 95. Got", example_vintage.get_age())
+    print("Peasant get_age() - Expected 4. Got", example_other.get_age())
+    print("Gibson is_vintage() - Expected True. Got", example_vintage.is_vintage())
+    print("Peasant is_vintage() - Expected False. Got", example_other.is_vintage())
+
+
+main()
